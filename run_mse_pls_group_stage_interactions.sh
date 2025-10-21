56e16ddc60a2f0bb4b6553b42cb51e42c59a2aac
shams-emad <emad_shams@sfu.ca>
2025-10-21 13:04:44 -0700

    Update and rename mse_pls_group_stage_interactions.sh to run_mse_pls_group_stage_interactions.sh

--- run_mse_pls_group_stage_interactions.sh
+++ run_mse_pls_group_stage_interactions.sh
@@ -10,4 +10,4 @@
 module load matlab/2023b.2
 mkdir -p logs
 
-matlab -nosplash -nodesktop -r "addpath('/home/eshams/scratch/test'); base_path = '/home/eshams/scratch/test/Dataset/NLF/NLF_Older_Cohort'; mse_pls_group_session_interactions(base_path); exit;"
+matlab -nosplash -nodesktop -r "addpath('/home/eshams/scratch/test'); base_path = '/home/eshams/scratch/test/Dataset/NLF/NLF_Older_Cohort'; mse_pls_group_stage_interactions(base_path); exit;"
